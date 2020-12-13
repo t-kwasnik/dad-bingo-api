52ed8582b694a8bf305e000760334f417320e26f
Ted Kwasnik <Ted@Teds-MacBook-Air.local>
2020-12-13 12:04:41 -0700

    added logging

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(function(req, res, next) {
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
-
+console.log(process.env.MONGODB_URI)
 // Connect to the database before starting the application server. 
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
